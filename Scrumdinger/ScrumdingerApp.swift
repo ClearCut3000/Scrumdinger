99404d40eea3360c0ab01a9b6aec3de07a54dd0d
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2023-01-04 19:09:31 +0300

    Set up navigation

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct ScrumdingerApp: App {
-    var body: some Scene {
-        WindowGroup {
-          ScrumsView(scrums: DailyScrum.sampleData)
-        }
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ScrumsView(scrums: DailyScrum.sampleData)
+      }
     }
+  }
 }
