0c873ef392b10f2bc87080774868ff7e38cc0aad
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2023-01-12 16:33:12 +0300

    Created a data store

---
+++ Scrumdinger/ScrumStore.swift
@@ -0,0 +1,22 @@
+//
+//  ScrumStore.swift
+//  Scrumdinger
+//
+//  Created by Николай Никитин on 12.01.2023.
+//
+
+import Foundation
+import SwiftUI
+
+class ScrumStore: ObservableObject {
+  @Published var scrums: [DailyScrum] = []
+
+  private static func fileURL() throws -> URL {
+    try FileManager.default.url(for: .documentDirectory,
+                                in: .userDomainMask,
+                                appropriateFor: nil,
+                                create: false)
+    .appendingPathComponent("scrums.data")
+  }
+}
+
