e99cd971740e50a3948e924681ab23e5618bb48e
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2023-01-17 12:48:52 +0300

    Displayed recording indicators

--- Scrumdinger/MeetingTimerView.swift
+++ Scrumdinger/MeetingTimerView.swift
@@ -12,6 +12,7 @@ struct MeetingTimerView: View {
   //MARK: - View Dependencies
   let speakers: [ScrumTimer.Speaker]
   let theme: Theme
+  let isRecording: Bool
   private var currentSpeaker: String {
     speakers.first(where: { !$0.isCompleted })?.name ?? "Someone"
   }
@@ -25,6 +26,10 @@ struct MeetingTimerView: View {
           Text(currentSpeaker)
             .font(.title)
           Text("is speaking")
+          Image(systemName: isRecording ? "mic" : "mic.slash")
+            .font(.title)
+            .padding(.top)
+            .accessibilityLabel(isRecording ? "with transcription" : "without transcription")
         }
         .accessibilityElement(children: .combine)
         .foregroundStyle(theme.accentColor)
@@ -47,6 +52,6 @@ struct MeetingTimerView_Previews: PreviewProvider {
                                                ScrumTimer.Speaker(name: "Cathy", isCompleted: false)]
   }
   static var previews: some View {
-    MeetingTimerView(speakers: speakers, theme: .yellow)
+    MeetingTimerView(speakers: speakers, theme: .yellow, isRecording: true)
   }
 }
